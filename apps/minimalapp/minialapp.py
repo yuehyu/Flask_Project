bef510d4e8e59436f09b3baa1ec1b08b710bdb35
AlanYueh <yuehyu1028@gmail.com>
2026-06-18 16:08:10 +0800

    Project Apps/minimalapp  設定日誌級別(Logging) 和 設置 Flask Debugtoolbar

--- apps/minimalapp/minialapp.py
+++ apps/minimalapp/minialapp.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for, redirect, request, flash
 from email_validator import validate_email, EmailNotValidError
-
+import logging
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "8Y3U1E0H28"
+app.logger.setLevel(logging.DEBUG)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+toolbar = DebugToolbarExtension(app)
+app.logger.critical("fatal error")
+app.logger.error("error")
+app.logger.warning("warning")
+app.logger.info("info")
+app.logger.debug("debug")
+
+
+
 
 # with app.test_request_context():
 #   print(url_for("index"))
